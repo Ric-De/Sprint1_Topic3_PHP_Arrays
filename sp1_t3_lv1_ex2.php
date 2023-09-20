67724d8f759750da2cbf030e150cf3a4b319ecfd
Ric-De <ricdellazari@gmail.com>
2023-09-20 17:54:11 +0200

    Exercise 2 finished successfully

--- sp1_t3_lv1_ex2.php
+++ sp1_t3_lv1_ex2.php
@@ -1,5 +1,18 @@
 <?php
 
+/*Exercise 2
+$X = array(10, 20, 30, 40, 50,60);
+Display the size of the previous array and then remove an element from it. After removing the element, the entire keys must be normalized (their indexes must be reorganized). Display the size of the array one last time.*/
 
+$x = array(10, 20, 30, 40, 50, 60);
+
+echo "The size of the previous array is ". count($x). "<br>"; //count($array) and sizeof($array) to exactly the same thing.
+
+//$sizeBytes = strlen(serialize($x));
+//echo "The size of the array is ". $sizeBytes. "<br>"; //This lines are only for testing purposes
+
+unset($x[1]);
+
+echo "The size of the previous array is ". sizeof($x). "<br>";
 
 ?>
